270ed73c794cb48083a594f4bac623a3e9bfb8d1
Ashley <choiag@vcu.edu>
2022-12-06 23:28:33 -0500

    new site update

--- portfolio/scripts.js
+++ portfolio/scripts.js
@@ -3,3 +3,11 @@
   $( function() {
     $( ".draggable" ).draggable();
   } );
+
+  // function boxToggle() {
+  //   // var box = document.getElementById("box");
+
+  //   // box.classList.toggle("active");
+  //   $("#box").toggleClass("menu-open");
+  //   $("body").toggleClass("menu-open");
+  //   }
